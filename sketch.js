ed1049b299a33c083da0f937aadc9bf4a9963e32
vishwapandya <57280185+vishwapandya@users.noreply.github.com>
2021-11-09 18:33:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,38 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
+var myengine;
+var myworld;
+var ball;
 
-
-function setup()
-{
+function setup() {
   createCanvas(400,400);
- 
 
+  myengine = Engine.create();
+  myworld = myengine.world;
+  
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+
+  ground = Bodies.rectangle(200,390,400,20,ground_options);
+  World.add(myworld,ground);
+
+  
+  rectMode(CENTER);
 }
+
 
 function draw() 
 {
-  background("pink");
-
-
-
+  background("purple");
+  Engine.update(myengine);
+  
+  
+  rect(ground.position.x,ground.position.y,400,20);
+ 
 }
-
